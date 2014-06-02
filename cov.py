527f627baab69df03c6648c721d9a2066c2719df
JC <thebigjc@gmail.com>
2014-06-02 11:03:00 -0400

    Added some optimization to momentum and cov

--- cov.py
+++ cov.py
@@ -6,6 +6,21 @@ from datetime import timedelta, datetime, date
 import numpy as np
 import scipy as sp
 import sys
+import os, os.path
+
+def load_stocks(stocks, start, end):
+    fn = "cache/%s-%s-%s.pkl" % ('_'.join(stocks), str(start), str(end))
+    if os.path.isfile(fn) and os.access(fn, os.R_OK):
+        print "Cached"
+        return pd.read_pickle(fn)
+
+    print "Fetching"
+    pnl = web.DataReader(stocks, "yahoo", start, end)
+    print "Saving"
+    pnl.to_pickle(fn)
+
+    return pnl
+
 
 def calc_sharpe(weights, returns):
     #reg = np.power(weights, 2).sum()
@@ -13,16 +28,21 @@ def calc_sharpe(weights, returns):
     if reg == 0.0:
         return 0.0
 
+    l = len(weights[weights > 0])
+
     weights = weights / weights.sum()
     port_rets = (returns * weights).sum(1)
     mean = port_rets.mean()
     if np.isnan(mean):
         print reg
     std = port_rets.std()
-    sharpe = np.sqrt(len(weights)) * (mean / std)
-    print sharpe, mean, std, reg
+    sharpe = mean / std
+#    print sharpe, mean, std, reg, l
 
-    return -sharpe + 0.1 * (reg-1) * (reg-1)
+    l = l - 6
+    l *= l
+
+    return 0.1 * reg + 0.1 * l - sharpe
 
 def get_opt_holdings_opt(returns):
     import scipy.optimize as opt
@@ -52,20 +72,24 @@ start = end - timedelta(days=300)
 #stocks = sorted(['BMO.TO', 'POT.TO', 'CBY.TO', 'G.TO', 'BCE.TO', 'TCL-A.TO', 'VSN.TO'])
 stocks = sorted(syms)
 
-dr = web.DataReader(stocks, "yahoo", start, end)
+dr = load_stocks(stocks, start, end) 
 
 adj_close = dr['Adj Close'].fillna(method='pad')
-sma200 = adj_close[-200:].mean() < adj_close.iloc[-1]
+sma200 = adj_close.iloc[-200:].mean() < adj_close.iloc[-1]
+adj_close = adj_close[sma200[sma200].index.values]
+
+momentum = adj_close.iloc[-1] / adj_close.iloc[-22*3] - 1
+stocks = momentum[momentum > 0].index.values
+
+adj_close = adj_close[stocks]
 
 returns = adj_close.pct_change()+1
-print returns.columns[sma200]
 
 log_returns = np.log(returns - 0.01/252)
 
 weights = get_opt_holdings_opt(log_returns[-22*3:])
 weights = weights / weights.sum()
-#weights = weights[weights > 0.01].order()
-weights = weights.order()
+weights = weights[weights > 0.01].order()
 
 print weights
 
@@ -76,24 +100,46 @@ stocks_w = weights[-COUNT:].index.values
 price = dr['Close'].iloc[-1]
 
 price = price[stocks_w]
-momentum = dr['Adj Close'][stocks_w].iloc[-1] / dr['Adj Close'][stocks_w].iloc[-22*3] - 1
-#momentum = momentum[momentum > 0]
 
-stocks = momentum.order()[-COUNT/2:].index.values
+print price
+
+stocks = momentum[stocks_w].order()[-COUNT/2:].index.values
 price = price[stocks]
 
-PORT_SIZE = 100000
+PORT_SIZE = 103415.00 - 3540.00
 
 port = PORT_SIZE * 1. / len(price) / price
 
-shares = sp.floor(port / 50 + 0.5)*50
+shares_low = sp.floor(port / 50)*50
+shares_high = sp.ceil(port / 50)*50
 
-port = pd.DataFrame(index=shares.index)
-port['shares'] = shares
+port = pd.DataFrame(index=shares_low.index)
+port['shares_low'] = shares_low
+port['shares_high'] = shares_high
 port['price'] = price
-port['book'] = port.shares * port.price
+port['book_low'] = port.shares_low * port.price
+port['book_high'] = port.shares_high * port.price
 port['momentum'] = momentum
 
-print port
+def bitfield(n):
+    return [1 if digit=='1' else 0 for digit in bin(n)[2:]]
 
-print log_returns[stocks_w].corr()
+min_diff = PORT_SIZE
+best_bf = [0] * len(port)
+
+for x in xrange(2 ** len(port)):
+    bf = bitfield(x)
+    bf = bf + [0,] * (len(port) - len(bf))
+    port['field'] = bf
+    diff = PORT_SIZE - (port.book_low * (1-port.field) + port.book_high * port.field).sum()
+    if abs(diff) < min_diff:
+        min_diff = diff
+        best_bf = bf
+        print x, diff, bf
+
+port['field'] = best_bf
+port['shares'] = port.shares_low * (1-port.field) + port.shares_high * port.field
+port['book'] = port.shares * port.price
+
+print port.sort()
+print port.book.sum()
--- momentum.py
+++ momentum.py
@@ -1,40 +1,73 @@
 __author__ = 'jchristensen'
 
 import pandas.io.data as web
+import pandas as pd
 from datetime import timedelta, datetime, date
 import numpy as np
+import scipy as sp
+import sys
+import os, os.path
+from scipy.optimize import brute
 
 end = date.today()
-#end = datetime(2014,3,1,0,0,0)
+#end = datetime(2014,5,1,0,0,0)
 start = end - timedelta(days=800)
 
-stocks = ['BLV', # Vanguard Long-Term Bond ETF
-    'XIC.TO', # iShares S&P/TSX Capped Composite Index ETF
-    #    'ZLB.TO', # BMO Low Volatility Canadian Equity ETF
-    #'XMV.TO', # iShares MSCI Canada Minimum Volatility Index ETF
-    'DBA', # DB Agriculture Fund
-    'DBC', # DB Commodity Index Tracking Fund
-    'EMB', # iShares J.P. Morgan USD Emerging Markets Bond ETF
-    'EWJ', # iShares MSCI Japan ETF
-    'GLD', # SPDR Gold Trust
-    'LQD', # iShares iBoxx $ Investment Grade Corporate Bond ETF
-    'PRF', # FTSE RAFI US 1000 Portfolio
-    'RWX', # SPDR DJ Wilshire Intl Real Estate
-    'VCIT', # Intermediate-Term Corporate Bond Index Fund
-    'VEU', # FTSE All World Ex US ETF
-    'VNQ', # REIT ETF
-    'TIP', # TIPS Bond ETF
-    'VWO' # Emerging Markets ETF
-    ]
+def load_stocks(stocks, start, end):
+    fn = "cache/%s-%s-%s.pkl" % ('_'.join(stocks), str(start), str(end))
+    if os.path.isfile(fn) and os.access(fn, os.R_OK):
+        print "Cached"
+        return pd.read_pickle(fn)
+
+    print "Fetching"
+    pnl = web.DataReader(stocks, "yahoo", start, end)
+    print "Saving"
+    pnl.to_pickle(fn)
+
+    return pnl
+
+def slippage(vals, price, closes):
+    total = vals * 50 * closes
+    if total.sum() > price.sum():
+        return float("inf")
+
+    obj = (total - price)**2
+    #print vals, obj.sum()
+
+    return obj.sum()
+
+if len(sys.argv) < 2:
+    stocks = ['BLV', # Vanguard Long-Term Bond ETF
+            'XIC.TO', # iShares S&P/TSX Capped Composite Index ETF
+        #    'ZLB.TO', # BMO Low Volatility Canadian Equity ETF
+        #'XMV.TO', # iShares MSCI Canada Minimum Volatility Index ETF
+        'DBA', # DB Agriculture Fund
+        'GSP', # 
+        'EMB', # iShares J.P. Morgan USD Emerging Markets Bond ETF
+        'EWJ', # iShares MSCI Japan ETF
+        'GLD', # SPDR Gold Trust
+        'LQD', # iShares iBoxx $ Investment Grade Corporate Bond ETF
+        'PRF', # FTSE RAFI US 1000 Portfolio
+        'RWX', # SPDR DJ Wilshire Intl Real Estate
+        'VCIT', # Intermediate-Term Corporate Bond Index Fund
+        'VEU', # FTSE All World Ex US ETF
+        'VNQ', # REIT ETF
+        'TIP', # TIPS Bond ETF
+        'VWO' # Emerging Markets ETF
+        ]
+else:
+    stocks = [x.strip() for x in open(sys.argv[1]).readlines()]
+    print stocks
 
 print (start,end)
 
-dlr = web.DataReader('DLR.TO', "yahoo", start, end)
+dlr = load_stocks(['DLR.TO'], start, end)
 
-dr = web.DataReader(stocks, "yahoo", start, end)
+dr = load_stocks(stocks, start, end)
 
 dlr = dlr['Adj Close'].fillna(method='pad') / 10
 adj_close = dr['Adj Close'].fillna(method='pad')
+close = dr['Close'].fillna(method='pad')
 
 not_ca = [s for s in stocks if not s.endswith('.TO')]
 
@@ -46,6 +79,7 @@ sma200 = sma200 < adj_close.iloc[-1]
 
 for s in not_ca:
     adj_close[s] *= dlr
+    close[s] *= dlr
 
 returns = np.log(adj_close.shift(1) / adj_close)
 cor = returns.corr()
@@ -53,7 +87,7 @@ cor = returns.corr()
 x = None
 
 for i in (1, 3, 6, 12):
-    a = np.log((adj_close.iloc[-1] / adj_close.iloc[-12*22]))
+    a = np.log((adj_close.iloc[-1] / adj_close.iloc[-i*22]))
     if x is None:
         x = a
     else:
@@ -68,10 +102,32 @@ x = x.order()
 
 x = x[-6:]
 
-print cor[np.abs(cor) > 0.9]
+norm = x/x.sum()
 
-#cor = cor[x.index]
+selected_stocks = x.index.values
 
-#print(cor[cor.abs() > 0.90])
+PORT_SIZE = 283251.57
 
-print(x)
+close = close.iloc[-1][selected_stocks]
+
+price = PORT_SIZE * norm
+shares = price / close
+
+shares_high = sp.ceil(shares / 50)
+shares_low = sp.floor(shares / 50)
+
+ranges = [slice(int(l-1), int(h+1)) for l,h in zip(shares_low, shares_high)]
+print ranges
+
+shares = brute(slippage, ranges, (price, close), finish=None)
+
+port = pd.DataFrame(index=price.index)
+port['lots'] = shares
+port['close'] = close
+port['price'] = price
+port['shares'] = port.lots * 50
+port['book'] = port.shares * port.close
+port['slippage'] = port.book - port.price
+
+print port
+print port.book.sum(), PORT_SIZE
